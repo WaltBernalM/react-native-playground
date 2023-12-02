f35347e669a3061f0d7a2261e7f1f87ab31c47f0
Walter Bernal <wltrbm@gmail.com>
2023-12-02 13:17:16 -0700

    Creation of Header and Footer components

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native"
+
+export default function Footer() {
+  return (
+    <View style={{ marginBottom: 10, backgroundColor: "#F4CE14" }}>
+      <Text
+        style={{
+          fontSize: 18,
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        All rights reserved by Little Lemon, 2022
+      </Text>
+    </View>
+  )
+}
