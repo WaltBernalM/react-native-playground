52201ab54b44cbd0381db1b5d8c63b66a416ef55
Walter Bernal <wltrbm@gmail.com>
2023-12-06 01:46:38 -0700

    Creation of WelcomeScreen component

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { View } from 'react-native';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import WelcomeScreen from './components/WelcomeScreen';
 
 export default function App() {
   return (
     <>
-      <View style={{
-        flex: 1,
-        backgroundColor: '#495E57'
-      }}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#495E57",
+        }}
+      >
         <Header />
+        <WelcomeScreen />
       </View>
-      <View style={{
-        backgroundColor: "#495E57"
-      }}>
+      <View style={{ backgroundColor: "#495E57" }}>
         <Footer />
       </View>
     </>
