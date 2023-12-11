6d6fce31d7c7360de91488ff594e8bbe8b261f29
Walter Bernal <wltrbm@gmail.com>
2023-12-10 23:37:21 -0700

    code refactore to use StyleSheet of react native in MenuItems component

--- components/MenuItems.jsx
+++ components/MenuItems.jsx
@@ -1,4 +1,4 @@
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 const menuItemsToDisplay = [
   "Hummus \n Moutabal \n Falafel \n Marinated Olives \n Kofta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta"
@@ -6,33 +6,35 @@ const menuItemsToDisplay = [
 
 export default function MenuItems() {
   return (
-    <View style={{ flex: 0.75 }}>
+    <View style={menuStyles.container}>
       <ScrollView
-        style={{
-          paddingHorizontal: 40,
-          paddingVertical: 40,
-          backgroundColor: "black"
-        }}
+        indicatorStyle={"white"}
+        horizontal={false}
+        style={menuStyles.innerContainer}
       >
-        <Text
-          style={{
-            color: 'white',
-            fontSize: 40,
-            flexWrap: 'wrap'
-          }}
-        >
-          View Menu
-        </Text>
-        <Text
-          style={{
-            color: '#F4CE14',
-            fontSize: 36
-          }}
-        >
-          {menuItemsToDisplay[0]}
-        </Text>
+        <Text style={menuStyles.headerText}>View Menu</Text>
+        <Text style={menuStyles.itemText}>{menuItemsToDisplay[0]}</Text>
       </ScrollView>
     </View>
   )
 }
 
+
+const menuStyles = StyleSheet.create({
+  container: {
+    flex: 0.75
+  },
+  innerContainer: {
+    padding: 40,
+    backgroundColor: 'greedn'
+  },
+  headerText: {
+    fontSize: 40,
+    flexWrap: "wrap",
+    color: 'white'
+  },
+  itemText: {
+    color: 'yellow',
+    fontSize: 30
+  }
+})
