3db08019de5a29f32fa5ca7696faf4b3329789c3
Walter Bernal <wltrbm@gmail.com>
2023-12-13 21:09:10 -0700

    render of Menu component in App

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import WelcomeScreen from './components/WelcomeScreen';
 import MenuItems from './components/MenuItems';
+import Menu from './components/Menu';
 
 export default function App() {
   return (
@@ -15,7 +16,8 @@ export default function App() {
       >
         <Header />
         <WelcomeScreen />
-        <MenuItems />
+        {/* <MenuItems /> */}
+        <Menu/>
       </View>
       <View style={{ backgroundColor: "#495E57" }}>
         <Footer />
