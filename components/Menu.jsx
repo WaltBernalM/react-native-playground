f751259a669a669016c30d35cb0eb2204b9d3040
Walter Bernal <wltrbm@gmail.com>
2023-12-14 09:39:29 -0700

    Addition of price in item to render inside SectionList

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -4,74 +4,76 @@ const menuItems = [
   {
     title: "Appetizers",
     data: [
-      "Hummus",
-      "Moutabal",
-      "Falafel",
-      "Marinated Olives",
-      "Kofta",
-      "Eggplant Salad",
-    ]
+      { name: "Hummus", price: "$5.00" },
+      { name: "Moutabal", price: "$5.00" },
+      { name: "Falafel", price: "$7.50" },
+      { name: "Marinated Olives", price: "$5.00" },
+      { name: "Kofta", price: "$5.00" },
+      { name: "Eggplant Salad", price: "$8.50" },
+    ],
   },
   {
     title: "Main Dishes",
     data: [
-      "Lentil Burger",
-      "Smoked Salmon",
-      "Kofta Burger",
-      "Turkish Kebab"
-    ]
+      { name: "Lentil Burger", price: "$10.00" },
+      { name: "Smoked Salmon", price: "$14.00" },
+      { name: "Kofta Burger", price: "$11.00" },
+      { name: "Turkish Kebab", price: "$15.50" },
+    ],
   },
   {
     title: "Sides",
     data: [
-      "Fries",
-      "Buttered Rice",
-      "Bread Sticks",
-      "Pita Pocket",
-      "Lentil Soup",
-      "Greek Salad",
-      "Rice Pilaf",
-    ]
+      { name: "Fries", price: "$3.00", id: "11K" },
+      { name: "Buttered Rice", price: "$3.00" },
+      { name: "Bread Sticks", price: "$3.00" },
+      { name: "Pita Pocket", price: "$3.00" },
+      { name: "Lentil Soup", price: "$3.75" },
+      { name: "Greek Salad", price: "$6.00" },
+      { name: "Rice Pilaf", price: "$4.00" },
+    ],
   },
   {
     title: "Desserts",
     data: [
-      "Baklava",
-      "Tartufo",
-      "Tiramisu",
-      "Panna Cotta"
-    ]
-  }
+      { name: "Baklava", price: "$3.00" },
+      { name: "Tartufo", price: "$3.00" },
+      { name: "Tiramisu", price: "$5.00" },
+      { name: "Panna Cotta", price: "$5.00" },
+    ],
+  },
 ]
 
 const Item = (props) => {
-  const { name } = props
+  console.log('props: ', props)
+  const { name, price } = props
   return (
-  <View style={menuStyles.innerContainer}>
-    <Text style={menuStyles.itemText}>{name}</Text>
-  </View>
-)}
+    <View style={menuStyles.innerContainer}>
+      <Text style={menuStyles.itemText}>{name}</Text>
+      <Text style={menuStyles.itemText}>{price}</Text>
+    </View>
+  )}
 
 const Footer = () => <Text style={menuStyles.footerText}>Little Lemon 2022</Text>
 
 const Separator = () => <View style={menuStyles.separator}></View>
 
 function Menu() {
-  const renderItem = (props) => {
-    const { item } = props
-    return <Item name={item} />
+  const renderItem = (sections) => {
+    const { item: { name, price } } = sections
+    return <Item name={name} price={price} />
   } 
 
-  const renderSectionHeader = (props) => {
-    const { section: {title}} = props
+  const renderSectionHeader = (sections) => {
+    const { section: {title}} = sections
     return <Text style={menuStyles.sectionHeader}>{title}</Text>
   }
 
   return (
     <View style={menuStyles.container}>
       <SectionList
-        sections={menuItems} // Section is referred as menuItems[i], it generates an object for each data in the section
-        keyExtractor={(item, index) => item + index} // Item is referred as menuItems[i].data
+        sections={menuItems}
+        keyExtractor={({ name }, index) => name + index}
         renderItem={renderItem} // Each object inside the array is treated as section, and the "data" of menuItems is treated as "data" for the created SectionList object
         renderSectionHeader={renderSectionHeader}
         ListFooterComponent={Footer}
@@ -89,6 +91,8 @@ const menuStyles = StyleSheet.create({
     paddingHorizontal: 40,
     paddingVertical: 20,
     backgroundColor: "#333333",
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   sectionHeader: {
     backgroundColor: "#fbdabb",
@@ -99,7 +103,7 @@ const menuStyles = StyleSheet.create({
   },
   itemText: {
     color: "#F4CE14",
-    fontSize: 32,
+    fontSize: 22,
   },
   separator: {
     borderBottomWidth: 1,
