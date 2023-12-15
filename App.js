d6d44a1449b72cc1b6d0656caa93bdbf8336e991
Walter Bernal <wltrbm@gmail.com>
2023-12-15 01:18:09 -0700

    Addition of Pressable component in App.js to toggle between WelcomeScreen and Menu custom components

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import WelcomeScreen from './components/WelcomeScreen';
@@ -6,8 +6,11 @@ import MenuItems from './components/MenuItems';
 import Menu from './components/Menu';
 import FeedbackForm from './components/FeedbackForm';
 import LoginScreen from './components/LoginScreen';
+import { useState } from 'react';
 
 export default function App() {
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
     <>
       <View
@@ -17,11 +20,19 @@ export default function App() {
         }}
       >
         <Header />
-        {/* <WelcomeScreen /> */}
+        {!showMenu && <WelcomeScreen />}
+        <Pressable
+          style={styles.button}
+          onPress={() => {
+            setShowMenu(!showMenu)
+          }}
+        >
+          <Text style={styles.buttonText}>{showMenu ? "Home" : "Menu"}</Text>
+        </Pressable>
+        {showMenu && <Menu />}
         {/* <MenuItems /> */}
-        {/* <Menu /> */}
         {/* <FeedbackForm/> */}
-        <LoginScreen/>
+        {/* <LoginScreen/> */}
       </View>
       <View style={{ backgroundColor: "#495E57" }}>
         <Footer />
@@ -29,3 +40,21 @@ export default function App() {
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    fontSize: 22,
+    padding: 10,
+    marginVertical: 8,
+    margin: 40,
+    backgroundColor: "#EDEFEE",
+    borderColor: "#EDEFEE",
+    borderWidth: 2,
+    borderRadius: 12,
+  },
+  buttonText: {
+    color: "#333333",
+    textAlign: "center",
+    fontSize: 32,
+  }
+})
