e1651727c22b40a609f9eec8f62dcbc3111a17bc
Walter Bernal <wltrbm@gmail.com>
2024-01-03 17:44:07 -0700

    Addition of Image component in WelcomeScreen

--- components/WelcomeScreen.jsx
+++ components/WelcomeScreen.jsx
@@ -1,8 +1,13 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native"
 
 export default function WelcomeScreen() {
   return (
     <ScrollView indicatorStyle={"white"} style={welcomeScreenStyle.container}>
+      <Image
+        style={welcomeScreenStyle.logo}
+        source={require("../img/littleLemonLogo.png")}
+      />
       <Text style={welcomeScreenStyle.haeding}>Welcome to Little Lemon</Text>
       <Text style={welcomeScreenStyle.textBody}>
         Little Lemon is a charming neighborhood bistro that serves simple food
@@ -18,7 +23,7 @@ const welcomeScreenStyle = StyleSheet.create({
     flex: 1,
   },
   haeding: {
-    paddingTop: 30,
+    paddingTop: 10,
     paddingHorizontal: 30,
     fontSize: 36,
     color: "#EDEFEE",
@@ -31,5 +36,11 @@ const welcomeScreenStyle = StyleSheet.create({
     marginVertical: 8,
     color: "#EDEFEE",
     textAlign: "center",
-  }
+  },
+  logo: {
+    height: "30%",
+    width: "100%",
+    resizeMode: "contain",
+    left: "42%",
+  },
 })
