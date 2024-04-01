77c68d2a26a0e3f94b3e00a47368b6bf3e0dbbeb
Walter Bernal <wltrbm@gmail.com>
2024-04-01 13:22:35 -0700

    feat: tab navigator

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import WelcomeScreen from "./screens/WelcomeScreen/Welcome.screen"
 import MenuScreen from "./screens/MenuScreen/Menu.screen"
 import Header from "./components/Header/Header.component"
 import Footer from "./components/Footer/Footer.component"
+import { Ionicons } from "@expo/vector-icons"
 
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
@@ -15,7 +16,7 @@ export default function App() {
     <>
       <Header />
       <NavigationContainer>
-        <Stack.Navigator
+        {/* <Stack.Navigator
           initialRouteName="Login"
           screenOptions={{
             headerStyle: { backgroundColor: "#333333" },
@@ -28,7 +29,30 @@ export default function App() {
           <Stack.Screen name="Login" options={{ title: "Login" }} component={LoginScreen} />
           <Stack.Screen name="Welcome" options={{ title: "Home" }} component={WelcomeScreen} />
           <Stack.Screen name="Menu" options={{ title: "Menu" }} component={MenuScreen} />
-        </Stack.Navigator>
+        </Stack.Navigator> */}
+        <Tab.Navigator
+          initialRouteName="Welcome"
+          screenOptions={({route}) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName
+
+              if (route.name === "Welcome") {
+                iconName = focused
+                  ? 'home'
+                  : 'home-outline'
+              } else if (route.name === 'Menu') {
+                iconName = 'list'
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray'
+          })}
+        >
+          <Tab.Screen name="Welcome" options={{ title: " Home" }} component={WelcomeScreen} />
+          <Tab.Screen name="Menu" options={{ title: "Menu" }} component={MenuScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
       <Footer />
     </>
